8b4536fb324d3a7abd6630040462420d828258cd
brishchket <bris26022006@gmail.com>
2026-06-19 17:37:57 +0530

    implemented authentication flow with user registration, login, and logout; added axios instance and auth store management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import useAuthStore from "./store/authStore.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const init = async () => {
+  await useAuthStore.getState().checkAuth();
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+};
+
+init();
