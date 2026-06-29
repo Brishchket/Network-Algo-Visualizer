fc583a323d2339946b23ad132dcf695fc3beff19
brishchket <bris26022006@gmail.com>
2026-06-29 14:26:53 +0530

    minor bug fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    commonjsOptions: {
-      transformMixedEsModules: true,   // ← fixes CJS/ESM mixing issues
-    },
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
   },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
-  },
-})
+};
